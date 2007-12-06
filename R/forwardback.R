c7499e2d2524a408a609fbdd60d46b08cd74065e
David Harte <david@statsresearch.co.nz>
2007-12-06 00:00:00 +0000

    version 1.2-3

--- R/forwardback.R
+++ R/forwardback.R
@@ -1,38 +1,53 @@
 "forwardback" <-
-function(x, Pi, delta, distn, pm, pn = NULL){
+function(x, Pi, delta, distn, pm, pn = NULL, fortran=TRUE){
     m <- nrow(Pi)
     n <- length(x)
     dfunc <- makedensity(distn)
-    prob <- NULL
-    for (k in 1:m) {
-        prob <- cbind(prob,
-                      dfunc(x=x, getj(pm, k), pn, log=FALSE))
-    }
+    prob <- matrix(as.double(0), nrow=n, ncol=m)
+    for (k in 1:m)
+        prob[,k] <- dfunc(x=x, getj(pm, k), pn, log=FALSE)
     #   forward probabilities alpha_ij
-    phi <- matrix(delta, nrow=1)
-    logalpha <- matrix(rep(NA, m*n), nrow=n)
-    lscale <- 0
-    for (i in 1:n){
-        if (i > 1) phi <- phi %*% Pi
-        phi <- phi %*% diag(prob[i,])
-        sumphi <- sum(phi)
-        phi <- phi/sumphi
-        lscale <- lscale + log(sumphi)
-        logalpha[i,] <- log(phi) + lscale
+    phi <- as.double(delta)
+    logalpha <- matrix(as.double(rep(0, m*n)), nrow=n)
+    lscale <- as.double(0)
+    if (fortran!=TRUE){
+        #  loop1 using R code
+        for (i in 1:n){
+            if (i > 1) phi <- phi %*% Pi
+            phi <- phi*prob[i,]
+            sumphi <- sum(phi)
+            phi <- phi/sumphi
+            lscale <- lscale + log(sumphi)
+            logalpha[i,] <- log(phi) + lscale
+        }
+        LL <- lscale
+    } else{
+        if (!is.double(Pi)) stop("Pi is not double precision")
+        if (!is.double(prob)) stop("prob is not double precision")
+        memory0 <- rep(as.double(0), m)
+        loop1 <- .Fortran("loop1", m, n, phi, prob, Pi, logalpha,
+                          lscale, memory0, PACKAGE="HiddenMarkov")
+        logalpha <- loop1[[6]]
+        LL <- loop1[[7]]
     }
-    #   log-likelihood
-    LL <- lscale
     #   backward probabilities beta_ij
-    logbeta <- matrix(rep(NA, m*n), nrow=n)
-    logbeta[n,] <- 0
-    phi <- matrix(rep(1/m, m), ncol=1)
-    lscale <- log(m)
-    for (i in seq(n-1, 1, -1)){
-        phi <- Pi %*% diag(prob[i+1,]) %*% phi
-        logbeta[i,] <- log(phi) + lscale
-        sumphi <- sum(phi)
-        phi <- phi/sumphi
-        lscale <- lscale + log(sumphi)
+    logbeta <- matrix(as.double(rep(0, m*n)), nrow=n)
+    phi <- as.double(rep(1/m, m))
+    lscale <- as.double(log(m))
+    if (fortran!=TRUE){
+        #  loop2 using R code
+        for (i in seq(n-1, 1, -1)){
+            phi <- Pi %*% (prob[i+1,]*phi)
+            logbeta[i,] <- log(phi) + lscale
+            sumphi <- sum(phi)
+            phi <- phi/sumphi
+            lscale <- lscale + log(sumphi)
+        }
+    } else{
+        memory0 <- rep(as.double(0), m)
+        loop2 <- .Fortran("loop2", m, n, phi, prob, Pi, logbeta,
+                          lscale, memory0, PACKAGE="HiddenMarkov")
+        logbeta <- loop2[[6]]
     }
     return(list(logalpha=logalpha, logbeta=logbeta, LL=LL))
 }
