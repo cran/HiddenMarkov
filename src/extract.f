44b03cf76b3fc59722e8b6b1d5678f3b4cf43e5d
David Harte <d.s.harte@gmail.com>
2016-08-30 07:48:00 +0000

    version 1.8-7

--- src/extract.f
+++ src/extract.f
@@ -2,8 +2,8 @@
       implicit none
 c     get the kth row from the (n*m) matrix a
 c     put into b
-      double precision a(n,m), b(m)
       integer m, n, j, k
+      double precision a(n,m), b(m)
       j = 1
       do while(j .le. m)
           b(j) = a(k,j)
@@ -16,8 +16,8 @@ c     put into b
       implicit none
 c     get the kth layer from array a
 c     put into b
-      double precision a(n,m,m), b(m,m)
       integer i, j, k, m, n
+      double precision a(n,m,m), b(m,m)
       i = 1
       do while(i .le. m)
           j = 1
